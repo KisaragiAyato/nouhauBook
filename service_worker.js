190e2b80366f126a6ffd247153df9974596d6bc5
KisaragiAyato <120772786+KisaragiAyato@users.noreply.github.com>
2024-04-10 20:51:14 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -18,49 +18,58 @@ for (var i = 0; i < nouhau.length; i++) {
   urlsToCache.push('./image/nouhau' + moji + '.png');
 }
 
-// インストール処理
-self.addEventListener('install', function(event) {
+//https://qiita.com/ichii731/items/0fb38333e8a0f00eb9ff より
+// キャッシュ追加
+self.addEventListener('install', function (event) {
+  console.log('ServiceWorker Install');
   event.waitUntil(
-    caches
-    .open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_VERSION)
+      .then(function (cache) {
+        console.log('cache.addAll');
+        cache.addAll(resources);
+      })
   );
 });
-
-// https://qiita.com/y_fujieda/items/f9e765ac9d89ba241154　より
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-              .then((response) => {
-                  if (response) {
-                      return response;
-                  }
-
-                  // 重要：リクエストを clone する。リクエストは Stream なので
-                  // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
-                  // 必要なので、リクエストは clone しないといけない
-                  let fetchRequest = event.request.clone();
-
-                  return fetch(fetchRequest)
-                      .then((response) => {
-                          if (!response || response.status !== 200 || response.type !== 'basic') {
-                              return response;
-                          }
-
-                          // 重要：レスポンスを clone する。レスポンスは Stream で
-                          // ブラウザ用とキャッシュ用の2回必要。なので clone して
-                          // 2つの Stream があるようにする
-                          let responseToCache = response.clone();
-
-                          caches.open(CACHE_NAME)
-                                .then((cache) => {
-                                    cache.put(event.request, responseToCache);
-                                });
-
-                          return response;
-                      });
-              })
-    );}
-);
+// キャッシュ表示
+self.addEventListener('fetch', function (event) {
+  console.log('ServiceWorker fetch');
+  event.respondWith(
+    // キャッシュが存在するかチェック
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        } else {
+          // キャッシュがない場合キャッシュに入れる
+          return fetch(event.request)
+            .then(function (res) {
+              return caches.open(DISP_VERSION)
+                .then(function (cache) {
+                  console.log('cache.put');
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                });
+            })
+            .catch(function () {
+              // 何もしない
+            });
+        }
+      })
+  );
+});
+// 古いキャッシュを削除
+self.addEventListener('activate', function (event) {
+  console.log('activate ServiceWorker');
+  event.waitUntil(
+    caches.keys()
+      .then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
+          if (key !== CACHE_VERSION && key !== DISP_VERSION) {
+            console.log('cache.delete');
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
+});
